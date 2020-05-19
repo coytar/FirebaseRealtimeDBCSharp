b618f1ec4b48a4a8105054f54d978bd16f0812f5
Raj Rao <raj.rao@plantweave.com.au>
2020-05-19 15:00:16 +1000

    Add project files.

---
+++ FirebaseRealtimeDBCSharp/Program.cs
@@ -0,0 +1,71 @@
+﻿using Firebase.Database;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirebaseRealtimeDBCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] scopes = { "https://www.googleapis.com/auth/firebase.database", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email" };
+            var firebase = new FirebaseClient(
+                "[URL]",
+                new FirebaseOptions
+                {
+                    AuthTokenAsyncFactory = () => GoogleAuthProvider.GetAccessTokenFromJSONKeyAsync("service-account.json", scopes),
+                    AsAccessToken = true
+                });
+
+            // Read example
+            Task[] t = new Task[2];
+            t[0] = ReadFromFirebase(firebase);
+
+            // Write example
+            t[1] = WriteToFirebase(firebase);
+
+            Task.WaitAll(t);
+        }
+
+        static async Task ReadFromFirebase(FirebaseClient firebase)
+        {
+            var val = await firebase.Child("users/temp").OnceSingleAsync<String>();
+            Console.WriteLine("Read: " + val);
+        }
+
+        static async Task WriteToFirebase(FirebaseClient firebase)
+        {
+            DateTime dt = DateTime.Now;
+
+            // Replace "users" node
+            //await firebase.Child("users").PutAsync("{ \"temp\": \"" + dt.ToShortDateString() + " " + dt.ToLongTimeString() + "\" }");
+
+            // Update "users" node
+            await firebase.Child("users").PatchAsync("{ \"temp\": \"" + dt.ToShortDateString() + " " + dt.ToLongTimeString() + "\" }");
+        }
+    }
+
+    public class GoogleAuthProvider
+    {
+        public static async Task<string> GetAccessTokenFromJSONKeyAsync(string jsonKeyFilePath, params string[] scopes)
+        {
+            using (var stream = new FileStream(jsonKeyFilePath, FileMode.Open, FileAccess.Read))
+            {
+                return await GoogleCredential
+                    .FromStream(stream) // Loads key file  
+                    .CreateScoped(scopes) // Gathers scopes requested  
+                    .UnderlyingCredential // Gets the credentials  
+                    .GetAccessTokenForRequestAsync()
+                    .ConfigureAwait(false); // Gets the Access Token  
+            }
+        }
+
+        public static string GetAccessTokenFromJSONKey(string jsonKeyFilePath, params string[] scopes)
+        {
+            return GetAccessTokenFromJSONKeyAsync(jsonKeyFilePath, scopes).Result;
+        }
+    }
+}
---
+++ FirebaseRealtimeDBCSharp/README.md
@@ -0,0 +1,23 @@
+﻿### FirebaseRealtimeDBCSharp
+A C# example of communicating with the Firebase Realtime database
+
+## NuGet packages required
+
+Install-Package Google.Apis.Auth
+Install-Package FirebaseDatabase.net
+
+### Package listing and versions
+
+Id                                  Versions
+--                                  --------
+FirebaseDatabase.net                {4.0.4}
+Google.Apis                         {1.45.0}
+Google.Apis.Auth                    {1.45.0}
+Google.Apis.Core                    {1.45.0}
+LiteDB                              {4.1.4} 
+Newtonsoft.Json                     {12.0.3}
+System.Reactive                     {4.0.0}
+
+## Test Environment
+Windows 10
+
